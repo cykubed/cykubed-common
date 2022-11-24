d66d9704e27d58d3e438176ac174c208f72d95ea
nick <nick@kisanhub.com>
2022-11-24 11:37:24 +0000

    Added Dockerfile

--- schemas.py
+++ schemas.py
@@ -1,15 +1,9 @@
 from datetime import datetime
 from typing import Optional, List
 
-import orjson
 from pydantic import BaseModel
 
 from .enums import PlatformEnum, Status, TestResultStatus
-
-
-def orjson_dumps(v, *, default):
-    # orjson.dumps returns bytes, to match standard json.dumps we need to decode
-    return orjson.dumps(v, default=default).decode()
 
 
 class OrganisationIn(BaseModel):
@@ -103,10 +97,6 @@ class TestResult(BaseModel):
 
 
 class SpecResult(BaseModel):
-    class Config:
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
-
     file: str
     tests: List[TestResult]
 
