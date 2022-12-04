280673c79fac3a37c44ead3fb5bc4b31c85a9924
nick <nick@kisanhub.com>
2022-12-04 15:43:21 +0000

    Switched to multipart file upload

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
+import datetime
 import os
 import subprocess
+from decimal import Decimal
+from json import JSONEncoder
 from sys import stdout
+from uuid import UUID
 
 
 def runcmd(cmd: str, logfile=None, **kwargs):
@@ -10,3 +14,13 @@ def runcmd(cmd: str, logfile=None, **kwargs):
     env = os.environ.copy()
     env['PATH'] = './node_modules/.bin:' + env['PATH']
     subprocess.check_call(cmd, shell=True, stderr=logfile, stdout=logfile, env=env, **kwargs)
+
+
+# subclass JSONEncoder
+class DateTimeEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat()
+        if isinstance(obj, UUID) or isinstance(obj, Decimal):
+            return str(obj)
+        return super().default(obj)
