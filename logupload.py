179aa2b4e4b01f95b19c35ee43b5cadd3028aba3
nick <nickbrook72@gmail.com>
2023-01-02 10:49:57 +0000

    helper to upload traceback

--- logupload.py
+++ logupload.py
@@ -1,6 +1,7 @@
 import tempfile
 import threading
 import time
+import traceback
 
 import httpx
 from loguru import logger
@@ -13,16 +14,24 @@ logthread: threading.Thread = None
 running = True
 
 
+def upload_exception_trace(trid: id):
+    upload_logs(trid, traceback.format_exc())
+
+
+def upload_logs(trid: int, data):
+    r = httpx.post(f'{settings.MAIN_API_URL}/agent/testrun/{trid}/logs',
+                   data=data, headers=get_headers())
+    if r.status_code != 200:
+        logger.error(f"Failed to push logs")
+
+
 def log_watcher(trid: int, fname: str):
     global running
     with open(fname, 'r') as logfile:
         while running:
             logs = logfile.read()
             if logs:
-                r = httpx.post(f'{settings.MAIN_API_URL}/agent/testrun/{trid}/logs',
-                               data=logs.encode('utf8'), headers=get_headers())
-                if r.status_code != 200:
-                    logger.error(f"Failed to push logs")
+                upload_logs(trid, logs.encode('utf8'))
             time.sleep(settings.LOG_UPDATE_PERIOD)
 
 
