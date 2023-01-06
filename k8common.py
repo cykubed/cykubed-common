67c54997e09f867b5ee5161b2730a0abe85ff2cb
nick <nick@kisanhub.com>
2023-01-06 14:47:15 +0000

    move template creation code to common default to k8

--- k8common.py
+++ k8common.py
@@ -1,22 +1,27 @@
 import os
 
-from kubernetes import client, config
+import pystache
+import yaml
+from kubernetes import client, config, utils
+from kubernetes.utils import FailToCreateError
+from loguru import logger
 
-batchapi = None
-NAMESPACE = 'cykube'
+from .schemas import NewTestRun
+
+NAMESPACE = os.environ.get('NAMESPACE', 'cykube')
 
 
-def get_batch_api() -> client.BatchV1Api:
-    global batchapi
+def init():
     if os.path.exists('/var/run/secrets/kubernetes.io'):
         # we're inside a cluster
         config.load_incluster_config()
-        batchapi = client.BatchV1Api()
     else:
         # we're not
         config.load_kube_config()
-        batchapi = client.BatchV1Api()
-    return batchapi
+
+
+def get_batch_api() -> client.BatchV1Api:
+    return client.BatchV1Api()
 
 
 def get_job_env():
@@ -27,3 +32,19 @@ def get_job_env():
              client.V1EnvVar(name='CACHE_URL', value='http://cache')
              ]
     return envs
+
+
+def create_jobs(templates_files, testrun: NewTestRun):
+    context = {
+        'image': testrun.project.runner_image,
+        'cpu': testrun.project.build_cpu,
+        'memory': testrun.project.build_memory,
+        'testrun_id': testrun.id,
+        'token': os.environ['API_TOKEN'],
+        'branch': testrun.branch
+    }
+    yaml_objects = [yaml.safe_load(pystache.render(f, context)) for f in templates_files]
+    try:
+        utils.create_from_yaml(client.ApiClient(), yaml_objects=yaml_objects)
+    except FailToCreateError:
+        logger.exception('Failed to create Job', trid=testrun.id)
