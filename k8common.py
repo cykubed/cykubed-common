2f802fb281338bdc24c2b756cf2a2968251edda8
nick <nickbrook72@gmail.com>
2023-01-10 16:31:32 +0000

    simplified k8 config

--- k8common.py
+++ k8common.py
@@ -6,17 +6,17 @@ batchapi = None
 NAMESPACE = 'cykube'
 
 
-def get_batch_api() -> client.BatchV1Api:
-    global batchapi
+def init():
     if os.path.exists('/var/run/secrets/kubernetes.io'):
         # we're inside a cluster
         config.load_incluster_config()
-        batchapi = client.BatchV1Api()
     else:
         # we're not
         config.load_kube_config()
-        batchapi = client.BatchV1Api()
-    return batchapi
+
+
+def get_batch_api() -> client.BatchV1Api:
+    return client.BatchV1Api()
 
 
 def get_job_env():
