671c76fc28d241af47ae7ef2bc8edb83eefa4efa
nick <nickbrook72@gmail.com>
2023-01-11 19:30:22 +0000

    Better job management and reporting

--- k8common.py
+++ k8common.py
@@ -25,6 +25,11 @@ def get_events_api() -> client.EventsV1Api:
     return client.EventsV1Api()
 
 
+@cache
+def get_core_api() -> client.CoreV1Api:
+    return client.CoreV1Api()
+
+
 def get_job_env():
     envs = []
     for key in ['API_TOKEN', 'MAIN_API_URL', 'AGENT_URL', 'CACHE_URL']:
