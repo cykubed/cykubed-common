dcc30d0bc4dd88bde38e45b52bd14af3a9607db8
nick <nickbrook72@gmail.com>
2023-01-12 12:27:58 +0000

    Switched to local_id

--- logupload.py
+++ logupload.py
@@ -1,33 +1,21 @@
-import traceback
-
 import httpx
 from loguru import logger
 
+from common import schemas
 from common.utils import get_headers
 from settings import settings
 
-logfile = None
-running = True
 
-
-def upload_exception_trace(trid: id):
-    upload_logs(trid, traceback.format_exc().encode('utf8'))
-
-
-def upload_log_line(trid: id, line: str):
-    logger.info(line)
-    upload_logs(trid, (line+"\n").encode('utf8'))
-
-
-def upload_logs(trid: int, data):
-    r = httpx.post(f'{settings.MAIN_API_URL}/agent/testrun/{trid}/logs',
+def upload_logs(project_id: int, local_id: int, data):
+    r = httpx.post(f'{settings.MAIN_API_URL}/agent/testrun/{project_id}/{local_id}/logs',
                    data=data, headers=get_headers())
     if r.status_code != 200:
         logger.error(f"Failed to push logs")
 
 
-def post_status(testrun_id: int, status: str):
-    resp = httpx.put(f'{settings.MAIN_API_URL}/agent/testrun/{testrun_id}/status/{status}', headers=get_headers())
+def post_testrun_status(tr: schemas.NewTestRun, status: str):
+    resp = httpx.put(f'{settings.MAIN_API_URL}/agent/testrun/{tr.project.id}/{tr.local_id}/status/{status}',
+                     headers=get_headers())
     if resp.status_code != 200:
-        raise Exception(f"Failed to update status for run {testrun_id}")
+        raise Exception(f"Failed to update status for run {tr.local_id} on project {tr.project.id}")
 
