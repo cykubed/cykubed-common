e7442d348c18346e8bd7e161dd4689c8627291a3
nick <nickbrook72@gmail.com>
2023-04-18 17:38:11 +0100

    Cancelling the pending tasks was causing an exception on shutdown - switched to a timeout and let the task end naturally

--- fsclient.py
+++ fsclient.py
@@ -10,8 +10,8 @@ import aioshutil
 from aiohttp import ClientError, ClientConnectionError
 from loguru import logger
 
-from common.exceptions import FilestoreWriteError, FilestoreReadError, BuildFailedException
-from common.settings import settings
+from .exceptions import FilestoreWriteError, FilestoreReadError, BuildFailedException
+from .settings import settings
 
 
 class AsyncFSClient(object):
