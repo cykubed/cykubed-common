d0ae2ea6a8c1238c119b8e373ca6948eedee2022
nick <nickbrook72@gmail.com>
2023-05-02 16:06:02 +0100

    Cast properly Moved dist into /build/dist for easier deleting

--- schemas.py
+++ schemas.py
@@ -267,7 +267,7 @@ class NewTestRun(BaseTestRun):
 class AgentTestRun(NewTestRun):
     node_cache_hit: bool = False
     cache_key: Optional[str]
-    specs: list[str]
+    specs: Optional[list[str]]
 
 
 class CacheItemType(str, enum.Enum):
