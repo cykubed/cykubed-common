738582ca62787c08ff314ae37f43f464594cf0c5
Nick Brook <nick@cykubed.com>
2023-06-06 11:44:56 +0100

    Switched to kubernetes_asyncio

--- k8common.py
+++ k8common.py
@@ -1,25 +1,34 @@
 import os
 
-from kubernetes import client, config
-from kubernetes.client import ApiClient
+from kubernetes_asyncio import client, config
+from kubernetes_asyncio.client import ApiClient
+
 
 NAMESPACE = os.environ.get('NAMESPACE', 'cykube')
 
 k8clients = dict()
 
 
-def init():
+async def init():
     if os.path.exists('/var/run/secrets/kubernetes.io'):
         # we're inside a cluster
-        config.load_incluster_config()
+        await config.load_incluster_config()
     else:
         # we're not
-        config.load_kube_config()
+        await config.load_kube_config()
     api = k8clients['api'] = ApiClient()
     k8clients['batch'] = client.BatchV1Api(api)
     k8clients['event'] = client.EventsV1Api(api)
     k8clients['core'] = client.CoreV1Api(api)
     k8clients['custom'] = client.CustomObjectsApi(api)
+
+
+async def close():
+    await get_client().close()
+
+
+def get_client() -> ApiClient:
+    return k8clients['api']
 
 
 def get_batch_api() -> client.BatchV1Api:
