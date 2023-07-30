76b70c181fa28e85818f02a283e26c1b62e1612d
nick <nickbrook72@gmail.com>
2023-07-30 14:14:01 +0100

    Can now make agents public if you are staff

--- schemas.py
+++ schemas.py
@@ -149,6 +149,7 @@ class UserProfile(BaseModel):
     email: str
     uisettings: UserUISettingsModel
     is_pending: bool
+    is_staff: Optional[bool] = False
     # allow_user_repositories: bool = False
     # integrations: list[IntegrationSummary]
     organisations: list[UserOrganisationSummary]
@@ -628,6 +629,7 @@ class UpdatedAgentModel(BaseModel):
     platform: Optional[KubernetesPlatform] = KubernetesPlatform.generic
     replication: str = 'singleton'
     service_account: Optional[str]
+    is_public: Optional[bool]
 
 
 class AgentModel(UpdatedAgentModel, NewAgentModel):
@@ -637,6 +639,7 @@ class AgentModel(UpdatedAgentModel, NewAgentModel):
     first_connected: Optional[datetime]
     version: Optional[str]
     connected: int = 0
+    is_public: Optional[bool]
 
     class Config:
         orm_mode = True
